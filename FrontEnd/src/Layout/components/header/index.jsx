fdeecdf2ebc2bfbc33d7efdf3f49dea9a1c1ff4f
TLphongNguyen <acelinhphong@gmail.com>
2024-08-05 16:51:24 +0700

    update ui Login

--- FrontEnd/src/Layout/components/header/index.jsx
+++ FrontEnd/src/Layout/components/header/index.jsx
@@ -1,13 +1,23 @@
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMoon, faPlus, faUserShield, faUserTie } from '@fortawesome/free-solid-svg-icons';
-
+import { Modal } from 'antd';
 import { HomeOutlined, TrophyOutlined, VideoCameraOutlined } from '@ant-design/icons';
-import { Dropdown } from 'antd';
 import Tippy from '@tippyjs/react';
 import { faBell } from '@fortawesome/free-regular-svg-icons';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 
 function Header() {
+	const [isModalOpen, setIsModalOpen] = useState(false);
+	const showModal = () => {
+		setIsModalOpen(true);
+	};
+	const handleOk = () => {
+		setIsModalOpen(false);
+	};
+	const handleCancel = () => {
+		setIsModalOpen(false);
+	};
 	return (
 		<div className="gwap-content h-[68px] py-[10px] px-[15px] border-b-[1px] border-solid border-[#dcdcdc]">
 			<div className="hearder-content flex justify-between">
@@ -130,13 +140,71 @@ function Header() {
 							<span className="text-[16px]">0 đ</span>
 						</a>
 					</div>
-					<div className="mx-[2px] bg-[#e8e8e8] rounded-[50%] w-[45px] h-[45px] text-center">
+					{/* <div className="mx-[2px] bg-[#e8e8e8] rounded-[50%] w-[45px] h-[45px] text-center">
 						<a className=" h-[45px] w-[45px] flex " href=" #">
 							<FontAwesomeIcon
 								className="text-[24px] items-center text-center m-auto hover:text-[#f0564a]"
 								icon={faUserTie}
 							/>
 						</a>
+					</div> */}
+					<div className="mx-[2px] bg-[#e8e8e8] rounded-[25px] leading-[45px] w-[110px] h-[45px] text-center">
+						<button onClick={showModal} className="font-[600]">
+							Đăng nhập
+						</button>
+						<Modal
+							width={'446px'}
+							height={'552px'}
+							open={isModalOpen}
+							onOk={handleOk}
+							onCancel={handleCancel}
+							footer={null}
+							closable={false}
+						>
+							<div className="py-[10px] px-[30px] h-[100%]">
+								<div className="mt-[30px] mb-[50px] text-center">
+									<img
+										className="w-[55px] bg-[#f0564a] rounded-[10px] mx-auto"
+										src="https://files.playerduo.net/production/static-files/logo.png"
+										alt=""
+									/>
+									<h1 className="my-[10px] text-[20px] text-[#f0564a] font-[600]">PlayerDuo</h1>
+								</div>
+								<form action="" className="">
+									<input
+										className="h-[42px] w-[100%] rounded-[10px] mb-[7px] mt-[15px] border-[1px] border-solid border-[#444] opacity-80 text-[#333] px-[15px]"
+										placeholder="Tên đăng nhập hoặc email"
+									/>
+									<input
+										className="h-[42px] w-[100%] rounded-[10px] mb-[7px] mt-[15px] border-[1px] border-solid border-[#444] opacity-80 text-[#333] px-[15px]"
+										placeholder="Mật khẩu"
+									/>
+									<span className="w-[100%] text-right block mt-[5px] mb-[10px]">
+										<a href="/" className="text-[#1e1f24] hover:text-[#f0564a]">
+											Quên mật khẩu?
+										</a>
+									</span>
+									<button
+										type="submit"
+										className="h-[42px] w-[100%] rounded-[10px] mb-[7px] mt-[25px] px-[15px] bg-[#f0564a] text-[#fff] text-[15px] font-[600] hover:bg-[#ef4b3f]"
+									>
+										Đăng nhập
+									</button>
+								</form>
+								<button
+									type="submit"
+									className="h-[42px] leading-[42px] w-[100%] rounded-[10px] mb-[7px] mt-[15px] px-[15px] border-[1px] border-solid border-[#444] text-[#444] text-[15px]"
+								>
+									<FontAwesomeIcon className="mr-2 text-[#3f5ead] text-[25px]" icon={faFacebook} />
+									Đăng nhập bằng FaceBook
+								</button>
+								<span className="mt-[60px] mb-[15px] w-[100%] text-center block">
+									<a href="" className="text-[#1e1f24] hover:text-[#f0564a] underline">
+										Đăng ký tài khoản
+									</a>
+								</span>
+							</div>
+						</Modal>
 					</div>
 					<div className="mx-[2px] border-l-1px border-solid border-[#ececec]  w-[45px] h-[45px] text-center">
 						<a className=" h-[45px] w-[45px] flex  hover:text-[#f0564a] items-center" href=" #">
